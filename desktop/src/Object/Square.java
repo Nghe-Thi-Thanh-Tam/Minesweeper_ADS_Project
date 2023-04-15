44efd2d3fbbd2bd54ac8560878b1a437e7778798
HuynhVanVinh <103840023+HuynhVanVinh@users.noreply.github.com>
2023-04-16 02:06:32 +0700

    update class Square().

--- desktop/src/Object/Square.java
+++ desktop/src/Object/Square.java
@@ -1,4 +1,43 @@
 package Object;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class Square {
+    int value;
+    boolean isOpened;
+    boolean isFlagged;
+    boolean isBomb;
+    Texture texture;
+
+    public void Square(){
+
+    }
+
+    public void update() {
+    }
+
+//    public final void render(SpriteBatch){
+//
+//    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value){
+        this.value = value;
+    }
+
+    public boolean isOpened() {
+        return this.isOpened;
+    }
+
+    public boolean isFlagged() {
+        return this.isFlagged;
+    }
+
+    public boolean isBomb() {
+        return this.isBomb;
+    }
 }
